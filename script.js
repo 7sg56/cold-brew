83a6ff94c7c23d1970fa67080b2d512cb95105c9
7sg56 <sghosh.ile.7@gmail.com>
2024-11-17 22:52:35 +0530

    update 1.2
    Added the typing animation using js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
+
+var typed=new Typed(".input",{
+    strings:["for hot days","made just for you","to fuel your grind","for cozy mornings"],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop:true   
+})
